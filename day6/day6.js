77dea24570c988cee847e781a4d078b455ed6154
Russell Benzing <me@russellbenzing.com>
2023-12-06 19:34:17 -0500

    solve day 6 part 1

--- day6/day6.js
+++ day6/day6.js
@@ -1,14 +1,30 @@
 const data = `
-Time:      7  15   30
-Distance:  9  40  200
+Time:        46     82     84     79
+Distance:   347   1522   1406   1471
 `;
 
 const [time, distance] = data.trim().split('\n');
 
 const timeArray = time.match(/\d+/gm).map(Number);
 const distanceArray = distance.match(/\d+/gm).map(Number);
+const dataSet = timeArray.map((time, idx) => ({
+    time: parseInt(time, 10),
+    distance: parseInt(distanceArray[idx], 10)
+}));
 
-var startSpeed = 0;
+console.log(dataSet);
 
-console.log(timeArray);
-console.log(distanceArray);
+const races = [];
+dataSet.forEach(({time, distance}) => {
+    let wins = 0;
+    for(let i = 0; i <= time; i++){
+        if(((time - i) * i) > distance) {
+            wins++;
+        }
+    }
+    races.push(wins);
+});
+
+let errorMargin = races.reduce((acc, cur) => acc * cur, 1);
+
+console.log(errorMargin);
