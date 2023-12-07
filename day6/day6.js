3681af05e6eba8ae1ac1e3da1d8195dd9c0b7d06
Russell Benzing <me@russellbenzing.com>
2023-12-06 19:48:06 -0500

    day 6 part 2

--- day6/day6.js
+++ day6/day6.js
@@ -5,15 +5,13 @@ Distance:   347   1522   1406   1471
 
 const [time, distance] = data.trim().split('\n');
 
+// Part 1
 const timeArray = time.match(/\d+/gm).map(Number);
 const distanceArray = distance.match(/\d+/gm).map(Number);
 const dataSet = timeArray.map((time, idx) => ({
     time: parseInt(time, 10),
     distance: parseInt(distanceArray[idx], 10)
 }));
-
-console.log(dataSet);
-
 const races = [];
 dataSet.forEach(({time, distance}) => {
     let wins = 0;
@@ -24,7 +22,16 @@ dataSet.forEach(({time, distance}) => {
     }
     races.push(wins);
 });
-
 let errorMargin = races.reduce((acc, cur) => acc * cur, 1);
-
 console.log(errorMargin);
+
+// Part 2
+const time2 = time.match(/\d+/gm).join('');
+const distance2 = distance.match(/\d+/gm).join('');
+let wins = 0;
+for(let i = 0; i <= time2; i++){
+    if(((time2 - i) * i) > distance2) {
+        wins++;
+    }
+}
+console.log(wins);
